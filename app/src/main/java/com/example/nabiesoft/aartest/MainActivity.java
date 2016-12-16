a466ab168ff0ee74c35e78028829103002ced78e
nabiesoft <nabiesoft@nabiesofts-MacBook-Pro.local>
2016-12-16 16:26:25 +0900

    test first cm

---
+++ app/src/main/java/com/example/nabiesoft/aartest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.nabiesoft.aartest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_test);
+
+        Button btnTest = (Button)findViewById(R.id.btnAarTest);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TestActivity.class));
+            }
+        });
+    }
+
+
+}
